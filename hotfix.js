bc4b4618f384481fd6a8e260c9c54a047ee8f8ad
phngskrkhawang@gmail.com <>
2024-07-24 17:30:12 +0700

    first commit

---
+++ hotfix.js
@@ -0,0 +1,15 @@
+git log --oneline --decorate --graph --all
+c2b9e(HEAD,master)Make other changes
+87ab2(testing)Make a change
+
+/
+f30ab add feature #32 - ability to add new formats to thecentral interface
+98ca9 lnitial commit of my project
+
+git init
+git checkout -b hotfix
+git config --global  --list
+git commit -a -m 'Fix broken email address'
+git checkout master
+git merge hotfix
+git branch -d hotfix
